6c2290d7b9aff0d999cc51592a76846b96ee29d9
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-03-07 22:50:33 +0630

    Webpage Category Show

--- index.php
+++ index.php
@@ -10,21 +10,47 @@
         <div class="col-lg-3 col-md-12">
             <ul class="list-group" style="margin-bottom: 40px;">
                 <li class="list-group-item active">Categrory</li>
-                <li class="list-group-item">Second item</li>
-                <li class="list-group-item">Third item</li>
+                <?php
+                    $sql = "SELECT * FROM category ORDER BY id DESC";
+                    $result = mysqli_query($connect,$sql);
+                    if($result){
+                        foreach ($result as $key => $value) {
+                            $id = htmlspecialchars($value['id']);
+                           ?>
+                                <li class="list-group-item">
+                                    <a href="category_detail.php?id=<?php echo $value['id'] ?>"> <?php echo $value['name'] ?> </a>
+                                </li>
+              
+                           <?php
+                        }
+                    }
+                ?>
+               
             </ul>
         </div>
         <div class="col-lg-9 col-md-12">
-            <div class="card col-lg-4" >
-                <img class="card-img-top" src="img/download (3).jpg" alt="Card image" style="width:100%">
-                <div class="card-body">
-                    <h4 class="card-title">John Doe</h4>
-                    <p class="card-text">Some example text.</p>
-                    <a href="#" class="btn btn-primary">See Profile</a>
+         <div class="row">
+        <?php
+                    $sql = "SELECT * FROM category ORDER BY id DESC";
+                    $result = mysqli_query($connect,$sql);
+                    if($result){
+                        foreach ($result as $key => $value) {
+                            $id = htmlspecialchars($value['id']);
+                            $image = "uploads/" . $value['image'];
+                           ?>
+            <div  class=" col-lg-5 col-md-12" style="height:200px;border-radius:0px;background-image:url('<?php echo $image ?>');margin-left:25px;margin-right:25px;margin-bottom:40px;background-size: cover; background-repeat: no-repeat; " >
+                <div >
+                   <p class="text-center " style="padding-top:20%; font-size:30px;font-weight:bold;color:black">  <?php echo $value['name'] ?> </p>
                 </div>
             </div>
+
+            <?php
+                        }
+                    }
+                ?>
         </div>
-       
+         <hr>
+        </div>
     </div>
 </div>
 
