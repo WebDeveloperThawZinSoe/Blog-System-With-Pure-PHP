b85d813651a3c14308d3f3c7b7614cef09faa735
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-03-10 22:03:14 +0630

    Left side bar change

--- components/left_category.php
+++ components/left_category.php
@@ -6,9 +6,12 @@
                     if($result){
                         foreach ($result as $key => $value) {
                             $id = htmlspecialchars($value['id']);
+                            $sql = "SELECT id FROM post WHERE category='$id'";
+                            $result = mysqli_query($connect,$sql);
+                            $count = mysqli_num_rows($result);
                            ?>
                                 <li class="list-group-item">
-                                    <a href="category_detail.php?id=<?php echo $value['id'] ?>"> <?php echo $value['name'] ?>  </a>
+                                    <a href="category_detail.php?id=<?php echo $value['id'] ?>"> <?php echo $value['name'] ?> <span class="badge badge-primary"><?php echo $count; ?></span>  </a>
                                 </li>
               
                            <?php
