8fa4466dc5df1df088996304c4ebda5d89c61372
Thaw <63851168+WebDeveloperThawZinSoe@users.noreply.github.com>
2022-03-10 22:42:32 +0630

    pagination

--- posts.php
+++ posts.php
@@ -14,18 +14,37 @@
                 <?php
                     include "components/left_category.php";
                 ?>
-               
+
             </ul>
         </div>
         <div class="col-lg-9 col-md-12">
-        
-         <hr><h2 style="text-decoration: underline;margin-bottom:30px">All Posts</h2>
-         <div class="row">
+
+            <hr>
+            <h2 style="text-decoration: underline;margin-bottom:30px">All Posts</h2>
+            <div class="row">
 
                 <br>
                 <?php
-                    $sql = "SELECT * FROM post  ORDER BY id DESC ";
-                    $result = mysqli_query($connect,$sql);
+
+$per_page = 20;
+
+
+##echo $number_of_page;
+
+if(!isset($_GET["page"])){
+    $page = 1;
+}else{
+    $page = $_GET["page"];
+}
+$sql1 = "SELECT * FROM post";
+$result = mysqli_query($connect,$sql1);
+$all_data = mysqli_num_rows($result);   
+                    
+$number_of_page =  ceil($all_data / $per_page);               
+$first_page_result = ($page - 1) * $per_page; //0 , 10  ,20
+$sql = "SELECT * FROM post LIMIT $first_page_result , $per_page ";
+$result = mysqli_query($connect,$sql);
+
                     if($result){
                         foreach ($result as $key => $value) {
                             $id = htmlspecialchars($value['id']);
@@ -35,7 +54,7 @@
                             $author = $value["author"];
                             $image = "uploads/" . $value['featured_image'];
                            ?>
-                    <div class="col-md-4">
+                <div class="col-md-4">
                     <div class="card">
                         <img src="<?php echo $image; ?>" alt="Avatar" style="width:100%;height:180px">
                         <div class="container">
@@ -51,21 +70,34 @@
                                 ?>
                             </p>
                             <br>
-                            <a href="blog_detail.php?id=<?php echo $id; ?>" class="btn btn-primary btn-block">Read More</a>
+                            <a href="blog_detail.php?id=<?php echo $id; ?>" class="btn btn-primary btn-block">Read
+                                More</a>
                             <br>
                         </div>
-                        </div>
                     </div>
+                </div>
 
-            <?php
+                <?php
                         }
                     }
                 ?>
 
-         </div>
+            </div>
+            <?php
 
-       
-         <br> <br>
+
+for($page = 1; $page <= $number_of_page;$page++){
+   
+    ?>
+            <a href='posts.php?page=<?php echo $page; ?>'> <?php echo $page; ?> | </a>
+            <?php
+}
+
+        ?>
+          
+
+
+            <br> <br>
         </div>
     </div>
 </div>
